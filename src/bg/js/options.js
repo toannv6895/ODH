418113f9c3edb64707651d1f614607a23e4539c0
Toan Nguyen <tnguyen@us.maxnerva.com>
2023-05-17 00:07:06 +0700

    add en=>vn cambridge

--- src/bg/js/options.js
+++ src/bg/js/options.js
@@ -68,7 +68,7 @@ function populateSysScriptsList(dictLibrary) {
     let systemscripts = [
         'builtin_encn_Collins', 'general_Makenotes',//default & builtin script
         'cncn_Zdic', //cn-cn dictionary
-        'encn_Collins', 'encn_Cambridge', 'encn_Cambridge_tc', 'encn_Oxford', 'encn_Youdao', 'encn_Baicizhan', //en-cn dictionaries
+        'encn_Collins', 'envn_Cambridge', 'encn_Cambridge', 'encn_Cambridge_tc', 'encn_Oxford', 'encn_Youdao', 'encn_Baicizhan', //en-cn dictionaries
         'enen_Collins', 'enen_LDOCE6MDX', 'enen_UrbanDict', //en-en dictionaries
         'enfr_Cambridge', 'enfr_Collins', //en-fr dictionaries
         'fren_Cambridge', 'fren_Collins', //fr-cn dictionaries
